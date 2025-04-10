80378d2fce7495f361dff8473e5569f9b2c2aa50
Jajak <149273701+Mikhail2141@users.noreply.github.com>
2025-04-10 23:17:37 +0300

    Добавлена Form3 Реализована выбор таблиц для добовляния в базу данных

--- Olimpiada/Form1.cs
+++ Olimpiada/Form1.cs
@@ -21,9 +21,12 @@ namespace Olimpiada
     {
         public Form1()
         {
+            StringCoonection();
             InitializeComponent();
             InitializingСomponents initializing = new InitializingСomponents();
             initializing.init(this);
+
+
         }
 
         // функция подключения к бд 
@@ -47,62 +50,45 @@ namespace Olimpiada
 
 
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            StringCoonection();
-        }
-
-
+      
         // Класс для добовления компонентов на форму 
         class InitializingСomponents
         {
             // Создание меню
             MenuStrip menuStrip = new MenuStrip();
-            ToolStripMenuItem fileMenu = new ToolStripMenuItem("Файл");
-            ToolStripMenuItem editMenu = new ToolStripMenuItem("Редактирование");
+            
+     
             ToolStripMenuItem reportsMenu = new ToolStripMenuItem("Отчеты");
             public ToolStripMenuItem helpMenu = new ToolStripMenuItem("Справка");
 
             // Добавление пунктов меню
             public void init(Form1 f1)
             {
-                fileMenu.DropDownItems.Add("Создать");
-                fileMenu.DropDownItems.Add("Открыть");
-                fileMenu.DropDownItems.Add("Сохранить");
-                fileMenu.DropDownItems.Add("Выход");
-
-                editMenu.DropDownItems.Add("Добавить");
-                editMenu.DropDownItems.Add("Удалить");
-                editMenu.DropDownItems.Add("Изменить");
+                 
+                 
 
                 reportsMenu.DropDownItems.Add("Таблица медального зачета");
                 reportsMenu.DropDownItems.Add("Медалисты");
                 reportsMenu.DropDownItems.Add("Статистика");
                  
-                menuStrip.Items.Add(fileMenu);
-                menuStrip.Items.Add(editMenu);
+                
                 menuStrip.Items.Add(reportsMenu);
                 menuStrip.Items.Add(helpMenu);
                 f1.Controls.Add(menuStrip);
 
-                // Добавляем обработчик события для "О программе"
-                ToolStripMenuItem aboutProgramMenuItem = new ToolStripMenuItem("О программе");
-                aboutProgramMenuItem.Click += new EventHandler(AboutProgram_Click);
-                helpMenu.DropDownItems.Add(aboutProgramMenuItem);
-
+               
 
                 // Создание панели инструментов
                 ToolStrip toolStrip = new ToolStrip();
                 ToolStripButton addButton = new ToolStripButton("Добавить");
                 ToolStripButton deleteButton = new ToolStripButton("Удалить");
                 ToolStripButton editButton = new ToolStripButton("Изменить");
-                ToolStripButton saveButton = new ToolStripButton("Сохранить");
+             
 
                 // Добавление кнопок на панель инструментов
                 toolStrip.Items.Add(addButton);
                 toolStrip.Items.Add(deleteButton);
                 toolStrip.Items.Add(editButton);
-                toolStrip.Items.Add(saveButton);
                 f1.Controls.Add(toolStrip);
 
                 // Создание таблицы для отображения данных
@@ -112,8 +98,23 @@ namespace Olimpiada
 
 
 
+
+
+
+                // Добавляем обработчик события для "О программе"
+                ToolStripMenuItem aboutProgramMenuItem = new ToolStripMenuItem("О программе");
+                aboutProgramMenuItem.Click += new EventHandler(AboutProgram_Click);
+                helpMenu.DropDownItems.Add(aboutProgramMenuItem);
+
+                addButton.Click += new EventHandler(ADD_Click);
+
+
+
+
+
             }
 
+            // Обработчики нажатия на меню 
 
             private void AboutProgram_Click(object sender, EventArgs e)
             {
@@ -121,6 +122,14 @@ namespace Olimpiada
                 Form2 form2 = new Form2();
                 form2.ShowDialog();
             }
+
+            private void ADD_Click(object sender, EventArgs e)
+            {
+                // Обработка нажатия на элемент меню "О программе"
+                Form3 form3 = new Form3();
+                form3.Show();
+            }
+
 
         }
 
---
+++ Olimpiada/Form3.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Olimpiada
+{
+    public partial class Form3 : Form
+    {
+        private Dictionary<int, Control[]> textBoxesByOption = new Dictionary<int, Control[]>();
+        private ListBox listBox;
+
+        public Form3()
+        {
+            InitializeComponent();
+            InitializeComponents();
+        }
+
+        private void InitializeComponents()
+        {
+            listBox = new ListBox
+            {
+                Location = new Point(10, 10),
+                Size = new Size(200, 120)
+            };
+
+            listBox.Items.AddRange(new object[]
+            {
+                "Добавить в таблицу спортсмены",
+                "Добавить в таблицу олимпиада",
+                "Добавить в таблицу спорт",
+                "Добавить в таблицу страны"
+            });
+
+            listBox.SelectedIndexChanged += OnSelectedIndexChanged;
+            Controls.Add(listBox);
+
+            CreateDefaultTextBoxes();
+        }
+
+        private void OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectedIndex = listBox.SelectedIndex;
+
+            RemoveExistingControls(selectedIndex);
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    CreateAthleteTextBoxes();
+                    break;
+                case 1:
+                    CreateOlympiadTextBoxes();
+                    break;
+                case 2:
+                    CreateSportTextBoxes();
+                    break;
+                case 3:
+                    CreateCountryTextBoxes();
+                    break;
+            }
+        }
+
+
+
+        private void RemoveExistingControls(int selectedIndex)
+        {
+            if (textBoxesByOption.TryGetValue(selectedIndex, out var controls))
+            {
+                foreach (var control in controls)
+                {
+                    Controls.Remove(control);
+                }
+                textBoxesByOption.Remove(selectedIndex);
+            }
+        }
+
+        private void CreateDefaultTextBoxes()
+        {
+            // Логика для создания стандартных текстовых полей при загрузке формы
+        }
+
+        private void CreateAthleteTextBoxes()
+        {
+            textBoxesByOption.Clear();
+
+            var textBoxes = new Control[]
+            
+            {
+                new TextBox { Name = "txtFIO", Location = new Point(300, 50), Size = new Size(100, 20) },
+                new TextBox { Name = "txtDate", Location = new Point(300, 80), Size = new Size(100, 20) },
+                new TextBox { Name = "txtCountry", Location = new Point(300, 110), Size = new Size(100, 20) }
+            };
+            
+            foreach (var textBox in textBoxes)
+            {
+                Controls.Add(textBox);
+            }
+             
+            textBoxesByOption[listBox.SelectedIndex] = textBoxes;
+        }
+
+        private void CreateOlympiadTextBoxes()
+        {
+            textBoxesByOption.Clear();
+            var textBoxes = new Control[]
+            {
+                new TextBox { Name = "Date", Location = new Point(300, 50), Size = new Size(100, 20) },
+                new TextBox { Name = "Season", Location = new Point(300, 80), Size = new Size(100, 20) },
+                new TextBox { Name = "City", Location = new Point(300, 110), Size = new Size(100, 20) },
+                new TextBox { Name = "HostContry", Location = new Point(300, 140), Size = new Size(100, 20) }
+            };
+
+            foreach (var textBox in textBoxes)
+            {
+                Controls.Add(textBox);
+            }
+
+            textBoxesByOption[listBox.SelectedIndex] = textBoxes;
+        }
+
+        private void CreateSportTextBoxes()
+        {
+            textBoxesByOption.Clear();
+            var textBoxes = new Control[]
+            {
+                new TextBox { Name = "Name", Location = new Point(300, 50), Size = new Size(100, 20) },
+               
+            };
+
+            foreach (var textBox in textBoxes)
+            {
+                Controls.Add(textBox);
+            }
+
+            textBoxesByOption[listBox.SelectedIndex] = textBoxes;
+        }
+
+        private void CreateCountryTextBoxes()
+        {
+            textBoxesByOption.Clear();
+            var textBoxes = new Control[]
+            {
+                new TextBox { Name = "Name", Location = new Point(300, 50), Size = new Size(100, 20) },
+                new TextBox { Name = "gold", Location = new Point(300, 80), Size = new Size(100, 20) },
+                new TextBox { Name = "silver", Location = new Point(300, 110), Size = new Size(100, 20) },
+                new TextBox { Name = "bronze", Location = new Point(300, 140), Size = new Size(100, 20) },
+                new TextBox { Name = "total medals", Location = new Point(300, 170), Size = new Size(100, 20) }
+            };
+
+            foreach (var textBox in textBoxes)
+            {
+                Controls.Add(textBox);
+            }
+
+            textBoxesByOption[listBox.SelectedIndex] = textBoxes;
+        }
+
+
+
+
+
+    }
+}
